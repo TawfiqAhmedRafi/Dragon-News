f0db8f5d349e5abc2c92c2f2595f36ccb220065e
TawfiqAhmedRafi <rafi70722@gmail.com>
2025-10-18 21:10:57 +0600

    sticky  scroll

--- src/Components/HomeLayout/FindUs.jsx
+++ src/Components/HomeLayout/FindUs.jsx
@@ -6,7 +6,7 @@ const FindUs = () => {
     <div>
       <h2 className="font-bold my-5">Find Us On </h2>
       <div>
-        <div className="join join-vertical lg:join-horizontal w-full">
+        <div className="join join-vertical  w-full">
           <button className="btn bg-base-100 justify-start join-item"><FaFacebook></FaFacebook> Facebook</button>
           <button className="btn bg-base-100 justify-start join-item"><FaTwitter></FaTwitter> Twitter</button>
           <button className="btn bg-base-100 justify-start join-item"><FaInstagram></FaInstagram> Instagram</button>
